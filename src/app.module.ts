1bfe660be9e76e27f35b2cb01ea1a4237f928320
Maria Castro Iregui <m.castroi@uniandes.edu.co>
2023-02-23 20:53:42 -0500

    postgre database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,38 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import AntecedenteEntity from './antecedente/antecedente.entity';
 import { AntecedenteModule } from './antecedente/antecedente.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContratoEntity } from './contrato/contrato.entity';
 import { ContratoModule } from './contrato/contrato.module';
-import { OfertaModule } from './oferta/oferta.module';
-import { ReseniaModule } from './resenia/resenia.module';
-import { UsuarioModule } from './usuario/usuario.module';
-import { NecesidadModule } from './necesidad/necesidad.module';
+import { EspecialidadEntity } from './especialidad/especialidad.entity';
 import { EspecialidadModule } from './especialidad/especialidad.module';
+import { HorarioEntity } from './horario/horario.entity';
 import { HorarioModule } from './horario/horario.module';
+import { NecesidadEntity } from './necesidad/necesidad.entity';
+import { NecesidadModule } from './necesidad/necesidad.module';
+import { OfertaEntity } from './oferta/oferta.entity';
+import { OfertaModule } from './oferta/oferta.module';
+import { ReseniaEntity } from './resenia/resenia.entity';
+import { ReseniaModule } from './resenia/resenia.module';
+import { UsuarioEntity } from './usuario/usuario.entity';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
-  imports: [OfertaModule, ContratoModule, AntecedenteModule, ReseniaModule, UsuarioModule, NecesidadModule, EspecialidadModule, HorarioModule,],
+  imports: [OfertaModule, ContratoModule, AntecedenteModule, ReseniaModule, UsuarioModule, NecesidadModule, EspecialidadModule, HorarioModule,
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'kangaroo',
+    entities: [UsuarioEntity, OfertaEntity, ContratoEntity, AntecedenteEntity, ReseniaEntity, NecesidadEntity, EspecialidadEntity, HorarioEntity],
+    dropSchema: true,
+    synchronize: true,
+    keepConnectionAlive: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
