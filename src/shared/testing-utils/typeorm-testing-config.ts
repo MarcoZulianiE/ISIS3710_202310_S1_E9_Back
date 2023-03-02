f366f423f01323eb18e0da476c27adc996c25090
Marco Zuliani <m.zuliani@uniandes.edu.co>
2023-03-02 10:14:34 -0500

    add shared files

---
+++ src/shared/testing-utils/typeorm-testing-config.ts
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AntecedenteEntity } from '../../antecedente/antecedente.entity';
+import { ContratoEntity } from '../../contrato/contrato.entity';
+import { EspecialidadEntity } from '../../especialidad/especialidad.entity';
+import { HorarioEntity } from '../../horario/horario.entity';
+import { NecesidadEntity } from '../../necesidad/necesidad.entity';
+import { OfertaEntity } from '../../oferta/oferta.entity';
+import { ReseniaEntity } from '../../resenia/resenia.entity';
+import { UsuarioEntity } from '../../usuario/usuario.entity';
+
+export const TypeOrmTestingConfig = () => [
+  TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: ':memory:',
+    dropSchema: true,
+    entities: [AntecedenteEntity, ContratoEntity, EspecialidadEntity, HorarioEntity, NecesidadEntity, OfertaEntity, ReseniaEntity, UsuarioEntity],
+    synchronize: true,
+    keepConnectionAlive: true 
+  }),
+  TypeOrmModule.forFeature([AntecedenteEntity, ContratoEntity, EspecialidadEntity, HorarioEntity, NecesidadEntity, OfertaEntity, ReseniaEntity, UsuarioEntity]),
+];
