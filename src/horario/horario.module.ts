caa08f93135e1123de56639ebc71d2d76e802e21
Maria Castro Iregui <m.castroi@uniandes.edu.co>
2023-03-03 08:54:28 -0500

    Logica y pruebas de horario

--- src/horario/horario.module.ts
+++ src/horario/horario.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HorarioEntity } from './horario.entity';
+import { HorarioService } from './horario.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([HorarioEntity])],
+  providers: [HorarioService]
+})
 export class HorarioModule {}
