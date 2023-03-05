84adb14b634b41daadb633ef4c912c9c2b37b908
Maria Castro Iregui <m.castroi@uniandes.edu.co>
2023-03-05 14:48:49 -0500

    fix module dependecy errors

--- src/especialidad/especialidad.module.ts
+++ src/especialidad/especialidad.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EspecialidadEntity } from './especialidad.entity';
 import { EspecialidadService } from './especialidad.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([EspecialidadEntity])],
   providers: [EspecialidadService]
 })
 export class EspecialidadModule {}
--- src/necesidad/necesidad.module.ts
+++ src/necesidad/necesidad.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NecesidadEntity } from './necesidad.entity';
+import { NecesidadService } from './necesidad.service';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([NecesidadEntity])],
+    providers: [NecesidadService]
+})
 export class NecesidadModule {}
