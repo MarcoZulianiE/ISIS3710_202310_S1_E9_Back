71c7bf34300efc363b347f4f3ebe1fcd662fedaa
Maria Castro Iregui <m.castroi@uniandes.edu.co>
2023-03-07 08:59:26 -0500

    contrato controller and dto

--- src/contrato/contrato.module.ts
+++ src/contrato/contrato.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OfertaEntity } from '../oferta/oferta.entity';
 import { ContratoEntity } from './contrato.entity';
 import { ContratoService } from './contrato.service';
+import { ContratoController } from './contrato.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ContratoEntity, OfertaEntity])],
-  providers: [ContratoService]
+  providers: [ContratoService],
+  controllers: [ContratoController]
 })
 export class ContratoModule {}
