1b00ae2d6d3e9378a8e4d8b9ce6a36a01f882b14
Maria Castro Iregui <m.castroi@uniandes.edu.co>
2023-03-07 17:18:34 -0500

    update oferta service to include composition

--- src/oferta/oferta.service.ts
+++ src/oferta/oferta.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UsuarioEntity } from 'src/usuario/usuario.entity';
 import { Repository } from 'typeorm';
 import { BusinessError, BusinessLogicException, NotFoundErrorMessage } from '../shared/errors/business-errors';
 import { OfertaEntity } from './oferta.entity';
@@ -8,7 +9,10 @@ import { OfertaEntity } from './oferta.entity';
 export class OfertaService {
     constructor(
         @InjectRepository(OfertaEntity)
-        private readonly ofertaRepository: Repository<OfertaEntity>
+        private readonly ofertaRepository: Repository<OfertaEntity>,
+
+        @InjectRepository(OfertaEntity)
+        private readonly usuarioRepository: Repository<UsuarioEntity>
     ) {}
 
     async findAll(): Promise<OfertaEntity[]> {
@@ -26,10 +30,18 @@ export class OfertaService {
 
     async create(oferta: OfertaEntity): Promise<OfertaEntity> {
         // TODO: revisar que fecha inicio sea menor que fecha fin
+
+        const usuario: UsuarioEntity = await this.usuarioRepository.findOne({where: {id: oferta.usuario.id}});
+
+        if(!usuario)
+            throw new BusinessLogicException(NotFoundErrorMessage("usuario"), BusinessError.NOT_FOUND);
+        
         
 
         if(oferta.tipoOferta.toLowerCase() != "canguro" && oferta.tipoOferta.toLowerCase() != "acudiente")
             throw new BusinessLogicException("El tipo de oferta debe ser 'canguro' o 'acudiente'", BusinessError.PRECONDITION_FAILED);
+
+        oferta.usuario = usuario;
         
         return await this.ofertaRepository.save(oferta);
     }
