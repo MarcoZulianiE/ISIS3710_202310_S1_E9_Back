212041e48740da4f0a8f996f7e65e2dd0255ca7e
Marco Zuliani <m.zuliani@uniandes.edu.co>
2023-03-07 22:04:15 -0500

    Add Usuario-Oferta Controller

--- src/usuario-oferta/usuario-oferta.module.ts
+++ src/usuario-oferta/usuario-oferta.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OfertaEntity } from 'src/oferta/oferta.entity';
 import { UsuarioEntity } from 'src/usuario/usuario.entity';
 import { UsuarioOfertaService } from './usuario-oferta.service';
+import { UsuarioOfertaController } from './usuario-oferta.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UsuarioEntity, OfertaEntity])],
-  providers: [UsuarioOfertaService]
+  providers: [UsuarioOfertaService],
+  controllers: [UsuarioOfertaController]
 })
 export class UsuarioOfertaModule {}
